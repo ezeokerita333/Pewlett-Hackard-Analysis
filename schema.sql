bdc321ac7d8ea28b5148114deaabffbbc70a8543
Rita Ezeoke <ezeokechibuzor333@gmail.com>
2022-10-12 14:51:16 -0400

    sql_queries

--- schema.sql
+++ schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE dept_manager (
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES title (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
@@ -42,17 +42,26 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
 );
 
 
 CREATE TABLE dept_emp(
+    
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES titles (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 		PRIMARY KEY (emp_no, dept_no)
 );
 	
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
